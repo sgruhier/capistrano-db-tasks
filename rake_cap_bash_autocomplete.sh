acfd1fdf2c9e70ee01edab1e3fa439c16a358bc1
sgruhier <sgruhier@mac.com>
2009-01-04 08:46:01 +0100

    much more simpler version

--- rake_cap_bash_autocomplete.sh
+++ rake_cap_bash_autocomplete.sh
@@ -1,26 +1,14 @@
+export COMP_WORDBREAKS=${COMP_WORDBREAKS/\:/}
+
 _rakecomplete() {
-  COMPREPLY=($(compgen -W "`rake -s -T | awk '{{print $2}}'`" -- ${COMP_WORDS[COMP_CWORD]}))
+  COMPREPLY=($(compgen -W "`rake -s -T 2>/dev/null | awk '{{print $2}}'`" -- ${COMP_WORDS[COMP_CWORD]}))
   return 0
 }
 
 _capcomplete() {
-  COMPREPLY=($(compgen -W "`cap  -T | awk '{{ if ( $3 ~ /\#/ ) print $2}}'`" -- ${COMP_WORDS[COMP_CWORD]}))
+  COMPREPLY=($(compgen -W "`cap  -T  2>/dev/null| awk '{{ if ( $3 ~ /\#/ ) print $2}}'`" -- ${COMP_WORDS[COMP_CWORD]}))
   return 0
 }
 
-cd () {
-    command cd "$@";
-    # Add enhanced completion for a folder containing a ruby env and remove it when we leave the folder                      
-    if [ -f ./Rakefile ]; then
-        complete -o default -o nospace -F _rakecomplete rake
-    else
-        complete -r rake  2>/dev/null
-    fi
-
-    # Add enhanced completion for a folder containing a capistrano env and remove it when we leave the folder                
-    if [ -f ./Capfile ]; then
-        complete -o default -o nospace -F _capcomplete cap
-    else
-        complete -r cap  2>/dev/null
-    fi
-}
+complete -o default -o nospace -F _rakecomplete rake
+complete -o default -o nospace -F _capcomplete cap
