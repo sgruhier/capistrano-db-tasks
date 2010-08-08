80a9642881849d5d6a2200ad694b2a7e78e7784f
Gabriel Andretta <ohhgabriel@gmail.com>
2010-08-08 17:25:09 -0300

    Adds completion for Thor.

--- rake_cap_bash_autocomplete.sh
+++ rake_cap_bash_autocomplete.sh
@@ -10,5 +10,11 @@ _capcomplete() {
   return 0
 }
 
+_thorcomplete() {
+  COMPREPLY=($(compgen -W "`THOR_COLUMNS=1000 thor -T 2>/dev/null| awk '{{ if ( $2 ~ /./ ) print $2}}'`" -- ${COMP_WORDS[COMP_CWORD]}))
+  return 0
+}
+
 complete -o default -o nospace -F _rakecomplete rake
 complete -o default -o nospace -F _capcomplete cap
+complete -o default -o nospace -F _thorcomplete thor
