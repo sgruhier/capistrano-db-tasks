47a96811112007e0386fa85b2b0567bd3aefb446
bigfive <jprdesign@gmail.com>
2012-06-12 15:49:57 +0800

    Added new asset pushing tasks

--- lib/dbtasks.rb
+++ lib/dbtasks.rb
@@ -43,10 +43,20 @@ Capistrano::Configuration.instance.load do |instance|
   end
   
   namespace :assets do
+    namespace :remote do
+      desc 'Synchronize your remote assets using local assets'
+      task :sync, :roles => :app do
+        puts "Assets directories: #{assets_dir}"
+        if Util.prompt "Are you sure you want to erase your server assets with local assets"
+          Asset.local_to_remote(instance)
+        end
+      end
+    end
+
     namespace :local do
       desc 'Synchronize your local assets using remote assets'
       task :sync, :roles => :app do
-        puts "Assets directories: #{Asset.to_string(instance)}"
+        puts "Assets directories: #{assets_dir}"
         if Util.prompt "Are you sure you want to erase your local assets with server assets"
           Asset.remote_to_local(instance)
         end
@@ -57,23 +67,44 @@ Capistrano::Configuration.instance.load do |instance|
     task :pull do 
       assets.local.sync
     end
+    
+    desc 'Synchronize the remote assets using local assets'
+    task :push do 
+      assets.remote.sync
+    end
   end
   
   namespace :app do
+    namespace :remote do
+      desc 'Synchronize your remote assets AND database using local assets and database'
+      task :sync do
+        if Util.prompt "Are you sure you want to REPLACE THE REMOTE DATABASE AND your remote assets with local database and assets(#{assets_dir})"
+          Database.local_to_remote(instance)
+          Asset.local_to_remote(instance)
+        end
+      end
+    end
+
     namespace :local do
       desc 'Synchronize your local assets AND database using remote assets and database'
       task :sync do
         puts "Local database     : #{Database::Local.new(instance).database}"
-        puts "Assets directories : #{Asset.to_string(instance)}"
+        puts "Assets directories : #{assets_dir}"
         if Util.prompt "Are you sure you want to erase your local database AND your local assets with server database and assets(#{assets_dir})"
           Database.remote_to_local(instance)
           Asset.remote_to_local(instance)
         end
       end
     end
+    
     desc 'Synchronize your local assets AND database using remote assets and database'
     task :pull do 
       app.local.sync
+    end
+
+    desc 'Synchronize your remote assets AND database using local assets and database'
+    task :push do 
+      app.remote.sync
     end
     
   end
