50430b478ae3a3483140ab1399cfe1debceb3653
Tim Kelty <timkelty@gmail.com>
2013-03-29 14:07:39 -0400

    Multiple local assets folders. Syncs to matching index in local_assets_dir.

--- lib/capistrano-db-tasks/asset.rb
+++ lib/capistrano-db-tasks/asset.rb
@@ -4,16 +4,16 @@ module Asset
   def remote_to_local(cap)
     servers = cap.find_servers :roles => :app
     port = cap.port rescue 22
-    [cap.assets_dir].flatten.each do |dir|
-      system("rsync -a --del -L -K -vv --progress --rsh='ssh -p #{port}' #{cap.user}@#{servers.first}:#{cap.current_path}/#{dir} #{cap.local_assets_dir}")
+    [cap.assets_dir].flatten.each_with_index do |dir, index|
+      system("rsync -a --del -L -K -vv --progress --rsh='ssh -p #{port}' #{cap.user}@#{servers.first}:#{cap.current_path}/#{dir} #{cap.local_assets_dir[index]}")
     end
   end
 
   def local_to_remote(cap)
     servers = cap.find_servers :roles => :app
     port = cap.port rescue 22
-    [cap.assets_dir].flatten.each do |dir|
-      system("rsync -a --del -L -K -vv --progress --rsh='ssh -p #{port}' ./#{dir} #{cap.user}@#{servers.first}:#{cap.current_path}/#{cap.local_assets_dir}")
+    [cap.assets_dir].flatten.each_with_index do |dir, index|
+      system("rsync -a --del -L -K -vv --progress --rsh='ssh -p #{port}' ./#{dir} #{cap.user}@#{servers.first}:#{cap.current_path}/#{cap.local_assets_dir[index]}")
     end
   end
 
