522bacfd06cd98aa6d63e8f7151e2cb5fd5efb04
Tim Kelty <timkelty@gmail.com>
2013-03-29 14:39:47 -0400

    Force array

--- lib/capistrano-db-tasks/asset.rb
+++ lib/capistrano-db-tasks/asset.rb
@@ -5,7 +5,7 @@ module Asset
     servers = cap.find_servers :roles => :app
     port = cap.port rescue 22
     [cap.assets_dir].flatten.each_with_index do |dir, index|
-      system("rsync -a --del -L -K -vv --progress --rsh='ssh -p #{port}' #{cap.user}@#{servers.first}:#{cap.current_path}/#{dir} #{cap.local_assets_dir.flatten[index]}")
+      system("rsync -a --del -L -K -vv --progress --rsh='ssh -p #{port}' #{cap.user}@#{servers.first}:#{cap.current_path}/#{dir} #{[cap.local_assets_dir].flatten[index]}")
     end
   end
 
@@ -13,7 +13,7 @@ module Asset
     servers = cap.find_servers :roles => :app
     port = cap.port rescue 22
     [cap.assets_dir].flatten.each_with_index do |dir, index|
-      system("rsync -a --del -L -K -vv --progress --rsh='ssh -p #{port}' ./#{dir} #{cap.user}@#{servers.first}:#{cap.current_path}/#{cap.local_assets_dir.flatten[index]}")
+      system("rsync -a --del -L -K -vv --progress --rsh='ssh -p #{port}' ./#{dir} #{cap.user}@#{servers.first}:#{cap.current_path}/#{[cap.local_assets_dir].flatten[index]}")
     end
   end
 
