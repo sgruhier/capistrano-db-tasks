4bce4f67f9f7cebfad02bd7968a535ebc094de51
Conor Hunt <conor.hunt+git@gmail.com>
2013-11-01 10:28:18 -0500

    allow mysql database.yml config to use 'user' as well as 'username'

--- lib/capistrano-db-tasks/database.rb
+++ lib/capistrano-db-tasks/database.rb
@@ -15,7 +15,8 @@ module Database
 
     def credentials
       if mysql?
-        (@config['username'] ? " -u #{@config['username']} " : '') + (@config['password'] ? " -p'#{@config['password']}' " : '') + (@config['host'] ? " -h #{@config['host']}" : '') + (@config['socket'] ? " -S#{@config['socket']}" : '')
+        username = @config['username'] || @config['user']
+        (username ? " -u #{username} " : '') + (@config['password'] ? " -p'#{@config['password']}' " : '') + (@config['host'] ? " -h #{@config['host']}" : '') + (@config['socket'] ? " -S#{@config['socket']}" : '')
       elsif postgresql?
         (@config['username'] ? " -U #{@config['username']} " : '') + (@config['host'] ? " -h #{@config['host']}" : '')
       end
